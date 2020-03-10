33f39f66b50034197f251dd7774a85018a6a22a3
Łukasz Piórkowski <lukasz.piorkowski@leaware.com>
2020-03-10 15:32:03 +0100

    Progress commit

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHendler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHendler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: enteredGoal }]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen} >
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input}
+          placeholder="Course Goal"
+          onChangeText={goalInputHendler}
+          value={enteredGoal} />
+        <Button title="Add" onPress={addGoalHendler} />
+      </View>
+      <FlatList 
+      keyExtractor={(item, index) => item.id}
+      data={courseGoals} 
+      renderItem={itemData =>
+        <View style={styles.listItem}>
+          <Text>{itemData.item.value}</Text>
+        </View>
+      }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1, padding: 10,
+    width: '80%',
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    margin: 10,
   },
 });
