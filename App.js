924b5e165dccb3f7a0f757020a0cba231b2c8f3e
40428516+pioro87@users.noreply.github.com <l.piorkowski87@gmail.com>
2020-03-10 18:22:56 +0100

    Progress

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from 'react-native';
+
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -10,7 +20,7 @@ export default function App() {
   };
 
   const addGoalHendler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: enteredGoal }]);
+    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: enteredGoal }]);
   };
 
   return (
@@ -22,38 +32,28 @@ export default function App() {
           value={enteredGoal} />
         <Button title="Add" onPress={addGoalHendler} />
       </View>
-      <FlatList 
-      keyExtractor={(item, index) => item.id}
-      data={courseGoals} 
-      renderItem={itemData =>
-        <View style={styles.listItem}>
-          <Text>{itemData.item.value}</Text>
-        </View>
-      }
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => 
+        <GoalItem title={itemData.item.value} />}
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  screen: {
-    padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    borderColor: 'black',
-    borderWidth: 1, padding: 10,
-    width: '80%',
-  },
-  listItem: {
-    padding: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1,
-    margin: 10,
-  },
-});
+  const styles = StyleSheet.create({
+    screen: {
+      padding: 50
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    },
+    input: {
+      borderColor: 'black',
+      borderWidth: 1, padding: 10,
+      width: '80%',
+    },
+  });
